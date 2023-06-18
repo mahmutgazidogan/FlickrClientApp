54aeabe3d9a190eb1861f8e8476cf0a7d2bf14e8
mahmutgazidogan <m.gazi.dogann>
2023-06-18 21:00:41 +0300

    files created and ui designed

---
+++ FlickrClientApp/Screens/RecentPhotos/Controller/RecentPhotosTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  FlickrClientApp
+//
+//  Created by Mahmut Gazi Doğan on 18.06.2023.
+//
+
+import UIKit
+
+class RecentPhotosTableViewController: UITableViewController, UISearchResultsUpdating {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let search = UISearchController()
+        search.searchResultsUpdater = self
+        search.obscuresBackgroundDuringPresentation = false
+        search.searchBar.placeholder = "Type something here to search"
+        navigationItem.searchController = search
+    }
+    
+    // MARK: - UITableViewDataSource & UITableViewDelegate
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PhotoTableViewCell
+        cell.ownerImageView.backgroundColor = .darkGray
+        cell.ownerNameLabel.text = "Owner Name"
+        cell.photoImageView.backgroundColor = .gray
+        cell.titleLabel.text = "Title Label"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let viewController = segue.destination as? PhotoDetailViewController {
+            
+        }
+    }
+    
+    // MARK: - UISearchResultsUpdating
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        if text.count > 2 {
+            print(text)
+        }
+    }
+
+}
+
---
+++ FlickrClientApp/Screens/RecentPhotos/View/PhotoTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoTableViewCell.swift
+//  FlickrClientApp
+//
+//  Created by Mahmut Gazi Doğan on 18.06.2023.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ownerImageView: UIImageView!
+    @IBOutlet weak var ownerNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
